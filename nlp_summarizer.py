ad05fed6833f2c1b91618b46ea60071864d409d6
insu <qkrdlstn9701@naver.com>
2023-09-19 11:53:10 +0900

    Update nlp_summarizer.py

--- nlp_summarizer.py
+++ nlp_summarizer.py
@@ -2,7 +2,12 @@ import streamlit as st
 from transformers import pipeline
 
 # Hugging Face Transformers를 사용하여 요약 모델을 불러옵니다.
-summarizer = pipeline("summarization")
+# summarizer = pipeline("summarization")
+
+@st.cache(allow_output_mutation=True)
+def load_summarizer():
+    model = pipeline("summarization", device=0)
+    return model
 
 # Streamlit 애플리케이션 설정
 # st.title("자연어 요약 앱")
