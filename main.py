120ed89f24c9d9888815c26819424389a1baa6aa
insu97 <52162653+insu97@users.noreply.github.com>
2023-09-22 22:41:03 +0900

    update ch02

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
 from common.util import preprocess, create_co_matrix, cos_similarity, most_similar, ppmi
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.title("밑바닥부터 시작하는 딥러닝2")
 
@@ -54,11 +54,9 @@ if CH02 :
             # 느린 code
             U, S, V = np.linalg.svd(W)
 
+        fig = px.scatter(x = U[:, 0], y = U[:, 1])
         for word, word_id in word_to_id.items():
-            plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
-
-        plt.scatter(U[:, 0], U[:, 1], alpha=0.5)
-        plt.show()
-
-
-
+            fig.add_annotation(text = word,
+                               x = U[word_id, 0],
+                               y = U[word_id, 1])
+        st.plotly_chart(fig)
