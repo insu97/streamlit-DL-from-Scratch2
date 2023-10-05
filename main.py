bc0760f847ade325f206cd0eb4977f1ff749aa2f
insu97 <52162653+insu97@users.noreply.github.com>
2023-10-05 16:17:40 +0900

    update CH03

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import numpy as np
 import streamlit as st
-from common.util import preprocess, create_co_matrix, cos_similarity, most_similar, ppmi
+from common.util import preprocess, create_co_matrix, cos_similarity, most_similar, ppmi, create_contexts_target, \
+    convert_one_hot
+from common.SimpleCBOW import SimpleCBOW
+from common.optimizer import Adam
+from common.trainer import Trainer
 import plotly.express as px
 
 st.title("밑바닥부터 시작하는 딥러닝2")
 
 CH02 = st.checkbox('CH02')
+CH03 = st.checkbox('CH03')
 
 if CH02 :
     text = st.text_input('영어문장을 입력해주세요.')
@@ -60,3 +65,44 @@ if CH02 :
                                x = U[word_id, 0],
                                y = U[word_id, 1])
         st.plotly_chart(fig)
+
+if CH03 :
+    text = st.text_input('영어문장을 입력해주세요.')
+    if text :
+        corpus, word_to_id, id_to_word = preprocess(text)
+        contexts, target = create_contexts_target(corpus, window_size=1)
+        st.write("contexts : %s" % contexts)
+        st.write("target : %s" % target)
+
+        vocab_size = len(word_to_id)
+        target = convert_one_hot(target, vocab_size)
+        contexts = convert_one_hot(contexts, vocab_size)
+
+        st.write("원-핫 벡터로 변경")
+        st.write("contexts")
+        st.write(contexts)
+        st.write("target : %s" % target)
+        
+        st.write('CBOW 모델 구현')
+        window_size = 1
+        hidden_size = 5
+        batch_size = 3
+        max_epoch = 1000
+
+        text = 'You say goodbye and I say hello.'
+        corpus, word_to_id, id_to_word = preprocess(text)
+
+        vocab_size = len(word_to_id)  # 7
+        contexts, target = create_contexts_target(corpus, window_size)
+        target = convert_one_hot(target, vocab_size)
+        contexts = convert_one_hot(contexts, vocab_size)
+        model = SimpleCBOW(vocab_size, hidden_size)
+        optimizer = Adam()
+        trainer = Trainer(model, optimizer)
+
+        trainer.fit(contexts, target, max_epoch, batch_size)
+        trainer.plot()
+
+        word_vecs = model.word_vecs
+        for word_id, word in id_to_word.items():
+            st.write(word, word_vecs[word_id])
